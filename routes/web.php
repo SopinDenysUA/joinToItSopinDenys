5a13dda5dbefbb0415da5bbd14c9218595d33a82
Denys <167997624+SopinDenysUA@users.noreply.github.com>
2025-01-14 18:30:20 +0200

    third commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,3 +13,7 @@ Route::middleware('admin')->group(function () {
     Route::get('/user', [UserController::class, 'index'])->name('user.index');
     Route::get('/admin', [UserController::class, 'adminPanel'])->name('user.admin');
 });
+
+Auth::routes(['register' => false]);
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
